1a61c29c9e6b22a1155c4237e0e5e5caeb3cf5e7
rosowskimik <m.rosowski1@wp.pl>
2019-06-12 17:06:23 +0200

    Create & Respond with JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -46,7 +48,22 @@ router.post(
 
       await user.save();
 
-      return res.send('User created');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       return res.status(500).json('Server error');
