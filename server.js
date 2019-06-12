ef5f2626cdd3cd846a9dda5e8bac89689a18c968
rosowskimik <m.rosowski1@wp.pl>
2019-06-12 15:42:02 +0200

    User model & Validation

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+//Connecting to Database
 const app = express();
 connectDB();
 
+//Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.json({ Aww: 'Working' });
+  res.send('Welcome to contact keep');
 });
 
 //Define Routes
