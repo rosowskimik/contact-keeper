27b852149a50bbb48e14859d669f0dcfedf57a48
rosowskimik <m.rosowski1@wp.pl>
2019-06-13 19:06:30 +0200

    Cleanup, Navbar & Router setup

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
