ca9bdfa5060cd16b5f274ea3a052475f8e92f22f
rosowskimik <m.rosowski1@wp.pl>
2019-06-20 17:47:15 +0200

    NotFound route & component

---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='form-container'>
+      <h1>Error 404</h1>
+      <p className='text-center'>Page not found</p>
+    </div>
+  );
+};
+
+export default NotFound;
