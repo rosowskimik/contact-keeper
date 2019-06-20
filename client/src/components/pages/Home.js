9a6b726ccf9f2dc235158917032d28ef7a3b904d
rosowskimik <m.rosowski1@wp.pl>
2019-06-20 19:04:48 +0200

    Fixed logout on page reload

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import ContactForm from '../contacts/ContactForm';
 import Contacts from '../contacts/Contacts';
 
+import AuthContext from '../../context/auth/authContext';
+
 const Home = () => {
+  const { token, loadUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    console.log(token);
+    loadUser(token);
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div className='container grid-2'>
       <div>
--- client/src/routing/PrivateRoute.js
+++ client/src/routing/PrivateRoute.js
@@ -4,22 +4,13 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthContext from '../context/auth/authContext';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const { isAuthenticated, loading } = useContext(AuthContext);
+  const { token, loading } = useContext(AuthContext);
 
   return (
     <Route
       {...rest}
       render={props =>
-        !isAuthenticated && !loading ? (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: isAuthenticated === null ? 'login' : 'logout'
-            }}
-          />
-        ) : (
-          <Component {...props} />
-        )
+        !token && !loading ? <Redirect to='/login' /> : <Component {...props} />
       }
     />
   );
